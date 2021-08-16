b187ff50711eefc1a4c942ed3dbb9aaad0a41f67
YaswanthPalika <yaswanthpalika19@ece.iiitp.ac.in>
2021-08-16 14:16:04 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter as Router} from 'react-router-dom'
 
 import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <h1>hello world</h1>
   </React.StrictMode>,
   document.getElementById('root'),
 )
